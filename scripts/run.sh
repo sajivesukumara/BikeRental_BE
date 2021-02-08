b51b9d309cfd548a6074e29628f60ca32b36e1ba
sajive-kumar <sajive.kumar@hpe.com>
2021-02-08 10:04:37 +0530

    Removed references to passwords and path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,13 @@
 __author__ = 'sajive'
-cd C:\Users\kumasaji\OneDrive - Hewlett Packard Enterprise\Sajive\Projects\BikeRentals\BikeRental_BE\app
+cd %MYPROJECT%\BikeRental_BE\app
 
 uvicorn app.main:app --reload
 
-docker run -p 5432:5432 --name rental_db -e POSTGRES_PASSWORD=hpinvent \
+docker run -p 5432:5432 --name rental_db -e POSTGRES_PASSWORD=%PGSQL_PASSWD% \
    -v ${HOME}/postgres-data/:/var/lib/postgresql/data -d postgres
 
 
-docker run -p 8080:80 -e 'PGADMIN_DEFAULT_EMAIL=sajive.sukumar@gmail.com' -e 'PGADMIN_DEFAULT_PASSWORD=hpinvent' --name dev-pgadmin -d dpage/pgadmin4
+docker run -p 8080:80 -e 'PGADMIN_DEFAULT_EMAIL=sajive.kumar@mail.com' -e 'PGADMIN_DEFAULT_PASSWORD=%PGSQL_PASSWD' --name dev-pgadmin -d dpage/pgadmin4
 
 
 docker inspect -f "{{ .Mounts }}" d98e907bd588
